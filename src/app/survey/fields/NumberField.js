4f5dbc50195ccfca5092c12e6d35babf2aaee64b
hfend <simplizine@gmail.com>
2020-03-25 19:58:13 +0100

    Support field suffix

--- src/app/survey/fields/NumberField.js
+++ src/app/survey/fields/NumberField.js
@@ -1,24 +1,34 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import { withTranslation } from 'react-i18next'
 
 import { errorMessage } from './../../utils/forms'
 
 
-function NumberField({t, question, register, errors}) {
+function NumberField({t, i18n, question, register, errors}) {
 
   return (
-    <div className="control">
-      <input
-        className={`input ${errors[question.key]?'is-danger':''}`}
-        name={question.key}
-        type="number"
-        step={question.format.kind==='decimal'?"0.01":"1"}
-        ref={register({
-          required: {value: true, message: errorMessage('required', t)()},
-          ...question.format.min!==undefined?{min: {value: question.format.min, message: errorMessage('min', t)(question.format.min) }}:{},
-          ...question.format.max!==undefined?{max: {value: question.format.max, message: errorMessage('max', t)(question.format.max) }}:{}
-        })}
-      />
+    <div class="field has-addons">
+      <p class="control">
+        <input
+          className={`input ${errors[question.key]?'is-danger':''}`}
+          name={question.key}
+          type="number"
+          step={question.format.kind==='decimal'?"0.01":"1"}
+          ref={register({
+            required: {value: true, message: errorMessage('required', t)()},
+            ...question.format.min!==undefined?{min: {value: question.format.min, message: errorMessage('min', t)(question.format.min) }}:{},
+            ...question.format.max!==undefined?{max: {value: question.format.max, message: errorMessage('max', t)(question.format.max) }}:{}
+          })}
+        />
+      </p>
+      {question.format[`suffix_${i18n.language}`] !== null && (
+        <p class="control">
+          <a class="button is-static">
+            {question.format[`suffix_${i18n.language}`]}
+          </a>
+        </p>
+      )}
       {errors[question.key] && (<p className="help is-danger">{errors[question.key].message}.</p>)}
     </div>
   );
--- src/app/survey/fields/TextField.js
+++ src/app/survey/fields/TextField.js
@@ -1,23 +1,33 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import { withTranslation } from 'react-i18next'
 
 import { errorMessage } from './../../utils/forms'
 
 
-function TextField({t, question, register, errors}) {
+function TextField({t, i18n, question, register, errors}) {
   
   return (
-    <div className="control">
-      <input
-        className={`input ${errors[question.key]?'is-danger':''}`}
-        name={question.key}
-        type="text"
-        ref={register({
-          required: {value: true, message: errorMessage('required', t)()},
-          ...question.format.min_length!==undefined?{minLength: {value: question.format.minLength, message: errorMessage('minLength', t)(question.format.minLength) }}:{},
-          ...question.format.max_length!==undefined?{maxLength: {value: question.format.maxLength, message: errorMessage('maxLength', t)(question.format.maxLength) }}:{}
-        })}
-      />
+    <div class="field has-addons">
+      <p class="control">
+        <input
+          className={`input ${errors[question.key]?'is-danger':''}`}
+          name={question.key}
+          type="text"
+          ref={register({
+            required: {value: true, message: errorMessage('required', t)()},
+            ...question.format.min_length!==undefined?{minLength: {value: question.format.minLength, message: errorMessage('minLength', t)(question.format.minLength) }}:{},
+            ...question.format.max_length!==undefined?{maxLength: {value: question.format.maxLength, message: errorMessage('maxLength', t)(question.format.maxLength) }}:{}
+          })}
+        />
+      </p>
+      {question.format[`suffix_${i18n.language}`] !== null && (
+        <p class="control">
+          <a class="button is-static">
+            {question.format[`suffix_${i18n.language}`]}
+          </a>
+        </p>
+      )}
       {errors[question.key] && (<p className="help is-danger">{errors[question.key].message}.</p>)}
     </div>
   );
