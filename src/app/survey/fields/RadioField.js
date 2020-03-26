fe18d2c6baa569710c06ce446e89fe65e0ea4960
hfend <simplizine@gmail.com>
2020-03-26 20:55:29 +0100

    Enhance radio style

--- src/app/survey/fields/RadioField.js
+++ src/app/survey/fields/RadioField.js
@@ -2,24 +2,27 @@ import React from 'react';
 import { withTranslation } from 'react-i18next'
 
 import { errorMessage } from './../../utils/forms'
+import { isRTL } from './../../i18n'
 
 
 function RadioField({t, i18n, question, register, errors}) {
   
   return (
-    <div className="control">
-      {question.format.choices.map((choice) => (
-        <label className="radio" key={choice.value} style={{display: 'block', margin: '0'}}>
+    <div>
+      {question.format.choices.map((choice, index) => (
+        <div className="field" key={choice.value}>
           <input
             type="radio"
+            className={`is-checkradio ${isRTL(i18n.language)?'is-rtl': ''}`}
+            id={`radio_${question.key}_${index}`}
             name={question.key}
             value={choice.value}
             ref={register({
               required: {value: true, message: errorMessage('required', t)()}
             })}
           />
-          {choice[`label_${i18n.language}`]}
-        </label>
+          <label for={`radio_${question.key}_${index}`}>{choice[`label_${i18n.language}`]}</label>
+        </div>
       ))}
       {errors[question.key] && (<p className="help is-danger">{errors[question.key].message}.</p>)}
     </div>
