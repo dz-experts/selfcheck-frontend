b6fff61885f9538813d94bf0fa18480c9bc36e74
hfend <simplizine@gmail.com>
2020-03-26 13:55:34 +0100

    Cleaner text direction change listener

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom'
 import { withTranslation } from 'react-i18next'
 import { renderRoutes } from "react-router-config"
@@ -7,72 +7,67 @@ import { renderRoutes } from "react-router-config"
 import { isRTL } from './i18n'
 
 
-class App extends React.Component {
+function App({t, i18n, route}) {
 
-  componentDidUpdate(prevProps) {
-    const direction = isRTL(this.props.i18n.language)?'rtl':'ltr'
-    if (document.body.dir !== direction) {
-      document.body.dir = direction
-    }
-  }
+  useEffect(() => {
+    // ensure page text direction follows language changes
+    document.body.dir = isRTL(i18n.language)?'rtl':'ltr'
+  });
 
-  render() {
-    const {t, i18n, route} = this.props
-    return (
-      <div>
-        
-        <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
-          <div className="container">
-            <div className="navbar-brand">
-              <Link to="/" className="navbar-item">
-                <strong>{t('Accueil')}</strong>
-              </Link>
+  return (
+    <div>
+      
+      <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
+        <div className="container">
+          <div className="navbar-brand">
+            <Link to="/" className="navbar-item">
+              <strong>{t('Accueil')}</strong>
+            </Link>
 
-              <a href="#" className="navbar-burger" aria-label="menu" aria-expanded="false">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-              </a>
-            </div>
-
-            <div className="navbar-end">
-              <a
-                href="#"
-                className={`navbar-item ${i18n.language === 'fr'?'is-active':''}`}
-                onClick={(e) => {
-                  i18n.changeLanguage('fr');
-                  e.preventDefault();
-                }}
-              >
-                Français
-              </a>
-              <a
-                href="#"
-                className={`navbar-item ${i18n.language === 'ar'?'is-active':''}`}
-                onClick={(e) => {
-                  i18n.changeLanguage('ar');
-                  e.preventDefault();
-                }}
-              >
-                العربية
-              </a>
-            </div>
+            <a href="#" className="navbar-burger" aria-label="menu" aria-expanded="false">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
           </div>
-        </nav>
 
-        {renderRoutes(route.routes)}
-
-        <footer className="footer">
-          <div className="content has-text-centered">
-            <p>
-              {t('Ce site d’information n’est pas un dispositif médical, il ne délivre pas d’avis médical.')}
-            </p>
+          <div className="navbar-end">
+            <a
+              href="#"
+              className={`navbar-item ${i18n.language === 'fr'?'is-active':''}`}
+              onClick={(e) => {
+                i18n.changeLanguage('fr');
+                e.preventDefault();
+              }}
+            >
+              Français
+            </a>
+            <a
+              href="#"
+              className={`navbar-item ${i18n.language === 'ar'?'is-active':''}`}
+              onClick={(e) => {
+                i18n.changeLanguage('ar');
+                e.preventDefault();
+              }}
+            >
+              العربية
+            </a>
           </div>
-        </footer>
+        </div>
+      </nav>
 
-      </div>
-    );
-  }
+      {renderRoutes(route.routes)}
+
+      <footer className="footer">
+        <div className="content has-text-centered">
+          <p>
+            {t('Ce site d’information n’est pas un dispositif médical, il ne délivre pas d’avis médical.')}
+          </p>
+        </div>
+      </footer>
+
+    </div>
+  );
 
 }
 
