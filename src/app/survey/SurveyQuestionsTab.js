3e6c03d0abae7971ddc4adbc2d7c37a2ea80d798
hfend <simplizine@gmail.com>
2020-03-26 18:16:56 +0100

    Contrast between the question text and the field

--- src/app/survey/SurveyQuestionsTab.js
+++ src/app/survey/SurveyQuestionsTab.js
@@ -70,12 +70,14 @@ function SurveyQuestionsTab({t, i18n, current_step, questions, answers, addAnswe
           {current_question[`text_${i18n.language}`]}
         </h2>
 
-        <div className="tile">
-          {current_question.format.type.toLowerCase() === 'text' && (<TextField question={current_question} register={register} errors={errors} />)}
-          {current_question.format.type.toLowerCase() === 'number' && (<NumberField question={current_question} register={register} errors={errors} />)}
-          {current_question.format.type.toLowerCase() === 'select' && (<SelectField question={current_question} register={register} errors={errors} />)}
-          {current_question.format.type.toLowerCase() === 'radio' && (<RadioField question={current_question} register={register} errors={errors} />)}
-        </div>
+        <article className="message">
+          <div className="message-body" style={{borderWidth: 0}}>
+            {current_question.format.type.toLowerCase() === 'text' && (<TextField question={current_question} register={register} errors={errors} />)}
+            {current_question.format.type.toLowerCase() === 'number' && (<NumberField question={current_question} register={register} errors={errors} />)}
+            {current_question.format.type.toLowerCase() === 'select' && (<SelectField question={current_question} register={register} errors={errors} />)}
+            {current_question.format.type.toLowerCase() === 'radio' && (<RadioField question={current_question} register={register} errors={errors} />)}
+          </div>
+        </article>
 
       </div>
 
