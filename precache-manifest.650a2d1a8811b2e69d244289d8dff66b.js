b08576d7eb72a053f4e49904883a31bb241a6dba
hfend <simplizine@gmail.com>
2020-03-26 09:09:29 +0100

    Updates

---
+++ precache-manifest.650a2d1a8811b2e69d244289d8dff66b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4572e64ba36e45d69e5e3f8d1911c95f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "515684cf9e0d53ab37e9",
+    "url": "/static/css/2.49233dca.chunk.css"
+  },
+  {
+    "revision": "bba5a9ba89423774f394",
+    "url": "/static/css/main.a9aa85bb.chunk.css"
+  },
+  {
+    "revision": "515684cf9e0d53ab37e9",
+    "url": "/static/js/2.e13024ac.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.e13024ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bba5a9ba89423774f394",
+    "url": "/static/js/main.6a1a8885.chunk.js"
+  },
+  {
+    "revision": "d3a987aa2652a996e921",
+    "url": "/static/js/runtime-main.b1fbfb2d.js"
+  }
+]);
