b1b0bfb25173d4e0910f55f1099c2a827eb9350c
Sohaib LAFIFI <sohaib.lafifi@univ-artois.fr>
2020-03-28 16:48:50 +0100

    Update text for result 3

--- src/app/self-check/SelfCheckResultsSeverity3.js
+++ src/app/self-check/SelfCheckResultsSeverity3.js
@@ -3,16 +3,17 @@ import { withTranslation } from 'react-i18next'
 
 
 function SelfCheckResultsSeverity3({t}) {
-  
+
   return (
     <article className="message is-warning">
       <div className="message-header">
         <p>{t('Vos symptômes nécessitent une prise en charge rapide.')}</p>
       </div>
-      <div className="message-body content">        
+      <div className="message-body content">
         <h4>{t('Appelez le')} <a href="tel:3030">30 30</a></h4>
+        <h4>{t('ou consulter le site du Ministère de la santé :')} <a href="http://www.sante.gov.dz">www.sante.gov.dz</a></h4>
         <p>
-          {t('Fournir plus d\'informations ici..')}
+          {t('Les services d\'urgence disposent des dernières procédures en vigueur dans votre zone géographique. Une prise en charge adaptée à votre région et à votre état de santé vous sera proposée.')}
         </p>
 
         <strong>{t('Restez chez vous.')}</strong>
